dc9873857a0a0b471636504d23f241bd409de1b4
skyaz <az.skyes@outlook.com>
2024-12-07 20:52:25 +0000

    updatinng ui code

--- artRepo/src/App.js
+++ artRepo/src/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import navMenu from './navMenu';
+
+
+function App() {
+    return (
+      <div>
+        <navMenu />
+      </div>
+    );
+  }
+  
+
+
+export default App;
---
+++ artRepo/src/navMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const navMenu = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a><span>HomePage</span></li>
+        <li><a href="#">Art</a><span>Compiler</span></li>
+        <li><a href="#">Contact</a><span>Reach Us.</span></li>
+        <li><a href="#">Inform.</a><span>ation</span></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default navMenu;
